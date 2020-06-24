8b5c4a420a44ce9d2d55c92a895604a4830ba136
pkp09 <pandeyprashant@live.in>
2020-06-25 00:35:43 +0530

    First commit

---
+++ springboot-restful-web-services/src/main/java/com/prashant/springboot/springbootrestfulwebservices/SpringbootRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.prashant.springboot.springbootrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulWebServicesApplication.class, args);
+	}
+
+}
