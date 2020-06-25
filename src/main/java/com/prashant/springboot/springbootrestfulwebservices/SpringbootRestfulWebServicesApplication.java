38dd9cd618523372cbb456c20fcccb603dad8448
pkp09 <pandeyprashant@live.in>
2020-06-25 18:40:15 +0530

    Internationalization added

--- src/main/java/com/prashant/springboot/springbootrestfulwebservices/SpringbootRestfulWebServicesApplication.java
+++ src/main/java/com/prashant/springboot/springbootrestfulwebservices/SpringbootRestfulWebServicesApplication.java
@@ -1,7 +1,13 @@
 package com.prashant.springboot.springbootrestfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class SpringbootRestfulWebServicesApplication {
@@ -9,5 +15,20 @@ public class SpringbootRestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestfulWebServicesApplication.class, args);
 	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		
+		return localeResolver;
+	}
 
+	
+	@Bean
+	public ResourceBundleMessageSource bundleMessageResource() {
+		ResourceBundleMessageSource bundleResource = new ResourceBundleMessageSource();
+		bundleResource.setBasename("messages");
+		return bundleResource;
+	}
 }
--- src/main/java/com/prashant/springboot/springbootrestfulwebservices/controller/RestTest.java
+++ src/main/java/com/prashant/springboot/springbootrestfulwebservices/controller/RestTest.java
@@ -1,11 +1,18 @@
 package com.prashant.springboot.springbootrestfulwebservices.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestTest {
+	@Autowired
+	MessageSource messageSource;
 
 	@GetMapping("/hello")
 	public String helloWorld() {
@@ -16,4 +23,11 @@ public class RestTest {
 	public String helloWorldPathVariable(@PathVariable String value) {
 		return "Hello : " + value;
 	}
+	
+	@GetMapping("/hello/i18n")
+	public String helloWorldInternationalization(@RequestHeader(name="Accept-Language", required=true) Locale locale) {
+		
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}
+	
 }
